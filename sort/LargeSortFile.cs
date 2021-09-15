b0c1bb678023f41bc22f46889c2f7078496abeee
Fedor Dobrovidov <dobrovidoff@gmail.com>
2021-09-15 12:56:15 +0300

    simple implementation of external sort

---
+++ sort/LargeSortFile.cs
@@ -0,0 +1,86 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Sort
+{
+    public class MergeSort
+    {
+        public const int ChunkSize = 4;
+
+        public void Sort(FileStream fileStream)
+        {
+            var fileIndex = 0;
+
+            using (var streamReader = new StreamReader(fileStream))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var chunk = new int?[ChunkSize]; 
+                    for (var i = 0; i < ChunkSize && !streamReader.EndOfStream; i++)
+                    {
+                        chunk[i] = int.Parse(streamReader.ReadLine());
+                    }
+
+                    Array.Sort(chunk);
+                    File.WriteAllLines($"chunks/{fileIndex++}.txt", chunk.Select(n => n.ToString()).Where(s => !string.IsNullOrEmpty(s)));
+                }
+            }
+
+            var buffer = new int?[ChunkSize];
+            var streamReaders = new StreamReader[ChunkSize];
+            for (var i = 0; i < ChunkSize; i++)
+            {
+                streamReaders[i] = new StreamReader(File.Open($"chunks/{i}.txt", FileMode.Open));
+            }
+
+            for (var i = 0; i < ChunkSize; i++)
+            {
+                var str = streamReaders[i].ReadLine();
+                if (!string.IsNullOrEmpty(str))
+                {
+                    buffer[i] =  int.Parse(str);
+                }
+            }
+
+            File.Delete("output/sorted.txt");
+            var chunksLeft = ChunkSize;
+            while (chunksLeft > 0)
+            {
+                int fileNameIndex = -1;
+                int min = int.MaxValue;
+                for (var i = 0; i < ChunkSize; i++)
+                {
+                    if (buffer[i] < min)
+                    {
+                        min = (int)buffer[i];
+                        fileNameIndex = i;
+                    }
+                }
+
+                File.AppendAllText("output/sorted.txt", $"{min.ToString()} ");
+                if (!streamReaders[fileNameIndex].EndOfStream)
+                {
+                    var str = streamReaders[fileNameIndex].ReadLine();
+                    if (!string.IsNullOrEmpty(str))
+                    {
+                        buffer[fileNameIndex] =  int.Parse(str);
+                    }
+                    else
+                    {
+                        chunksLeft--;
+                    }
+                }
+                else
+                {
+                    buffer[fileNameIndex] =  null; 
+                    chunksLeft--;
+                }
+            }
+        }
+    }
+}
+
+// O(N * log(K)) - sort chunks
+// O(N * K) - find min number N times in a buffer of size K
+// O(N * K + N * log(K))
---
+++ sort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Sort
+{
+    class Program
+    {
+        private const string FilePath = "input/nums.txt";
+
+        static void Main(string[] args)
+        {
+            var inputFileStream = File.Open(FilePath, FileMode.Open);
+          
+            var mergeSort = new MergeSort();
+            mergeSort.Sort(inputFileStream);
+            inputFileStream.Close();
+        }
+    }
+}
